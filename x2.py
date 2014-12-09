e66d57c4dc138f8f3818e91774b9b2a0d88067ae
themythicaldrago <ryan.louie@students.olin.edu>
2014-12-08 23:57:59 -0500

    x2 plotting train test

--- x2.py
+++ x2.py
@@ -4,8 +4,19 @@ import matplotlib.pyplot as plt
 trX = np.linspace(-5, 5, 101)
 trY = trX ** 2 + np.random.randn(*trX.shape) * 1.3 # noise for training
 
-# plt.plot(trY, '.')
-# plt.show()
 
 teX = np.linspace(-7, 7, 101)
 teY = teX ** 2 # no noise for testing
+
+plt.hold('on')
+plt.plot(trX, trY, 'r.')
+plt.plot(teX, teY, 'b-')
+plt.show()
+
+tru = trX.reshape(-1, 1)
+trt = trY.reshape(-1, 1)
+teu = teX.reshape(-1, 1)
+tet = teX.reshape(-1, 1)
+
+print tru.shape
+
